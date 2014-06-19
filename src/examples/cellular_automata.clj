c2ee5ec3aa0d5d126bd27aab8910f0d7b1a31353
Tom Savage <tcsavage@gmail.com>
2014-06-19 23:12:01 +0100

    Rename `examples.cellular-automata` file

--- src/examples/cellular_automata.clj
+++ src/examples/cellular_automata.clj
@@ -26,6 +26,7 @@
          :left (take 10 (repeat 0))
          :right (take 10 (repeat 0))})
 
-(with-comonad universe-w
-  (doseq [i (take 10 (iterate (partial w-extend (make-ca rule-110)) u0))]
-    (println (u-show 9 i))))
+(defn run-cellular-automaton
+  (with-comonad universe-w
+    (doseq [i (take 10 (iterate (partial w-extend (make-ca rule-110)) u0))]
+      (println (u-show 9 i)))))
