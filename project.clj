7c84b7585c4db7e3a21210f3ccc98e3d02233f90
Tom Savage <tcsavage@gmail.com>
2014-06-19 23:08:19 +0100

    Update project file

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject algo.comonads "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject tcsavage/algo.comonads "0.1.0-SNAPSHOT"
+  :description "Comonads for Clojure"
+  :url "https://github.com/tcsavage/algo.comonads"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.macro "0.1.2"]]
-  :plugins [[codox "0.8.7"]])
+                 [org.clojure/tools.macro "0.1.2"]])
